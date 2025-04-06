55c47c5775c0563136e3f4bf5903fc9170daa634
paulriya9073 <paulriya9073@gmail.com>
2025-04-06 13:53:54 +0530

    fixed:backgound

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -42,7 +42,7 @@ const AnimatedBackground = () => {
             linear-gradient(to right, rgba(255,255,255,0.1) 1px, transparent 1px),
             linear-gradient(to bottom, rgba(255,255,255,0.1) 1px, transparent 1px)
           `,
-          backgroundSize: '50px 50px',
+          backgroundSize: '30rem 3rem',
         }}
       />
 
